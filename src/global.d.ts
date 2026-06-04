87b09ee14d05d125f0ce06a21d246b28106ade61
Dome <dm@ztfr.de>
2026-06-04 11:38:22 +0200

    Update global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,3 +2,7 @@ declare const ZOCIAL_IS_SERVICE_WORKER: boolean
 declare const ZOCIAL_IS_BROWSER: boolean
 declare const ZOCIAL_VERSION: string
 declare const ZOCIAL_ENV: string
+
+interface Window {
+  __ZOCIAL_SINGLE_INSTANCE__?: string
+}
